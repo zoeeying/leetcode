1a2513501d7cdbcf753dbb2451489111c4ca21b8
zoeying <zoeeying@gmail.com>
2020-09-08 23:35:05 +0800

    完成第506题

---
+++ 343.整数拆分.js
@@ -0,0 +1,16 @@
+/*
+ * @lc app=leetcode.cn id=343 lang=javascript
+ *
+ * [343] 整数拆分
+ */
+
+// @lc code=start
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var integerBreak = function(n) {
+
+};
+// @lc code=end
+
--- 506.相对名次.js
+++ 506.相对名次.js
@@ -9,28 +9,20 @@
  * @param {number[]} nums
  * @return {string[]}
  */
-// var findRelativeRanks = function (nums) {
-//   // 找出n在nums中排名第几
-//   var findRank = (nums, n) => {
-//     let arr = JSON.parse(JSON.stringify(nums))
-//     // 从大到小排列
-//     arr.sort((a, b) => b - a)
-//     return arr.indexOf(n) + 1
-//   }
-
-//   let result = []
-//   for (let i = 0; i < nums.length; i++) {
-//     result.push(findRank(nums, nums[i]))
-//   }
-//   let hash = {
-//     1: 'Gold Medal',
-//     2: 'Silver Medal',
-//     3: 'Bronze Medal',
-//   }
-//   return result.map((item) => (hash[item] ? hash[item] : item + ''))
-// }
-
-var findRelativeRanks = function (nums) {}
+var findRelativeRanks = function (nums) {
+  const arr = [...nums]
+  arr.sort((a, b) => b - a)
+  const hash = {
+    1: 'Gold Medal',
+    2: 'Silver Medal',
+    3: 'Bronze Medal',
+  }
+  for (let i = nums.length - 1; i >= 0; i--) {
+    rank = arr.indexOf(nums[i]) + 1
+    nums[i] = hash[rank] ? hash[rank] : rank + ''
+  }
+  return nums
+}
 findRelativeRanks([5, 4, 3, 2, 1])
 
 // @lc code=end
